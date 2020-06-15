3c63c365ace94ef34d0aed7b09f3dcd751aa10ad
unknown <gkdogra1111@gmail.com>
2020-06-15 22:00:56 +0530

    show country flags

---
+++ index.js
@@ -0,0 +1,24 @@
+const express=require("express")
+
+const app=express()
+const novelcovid=require("novelcovid")
+const handlebars=require("express-handlebars")
+novelcovid.all().then(response=>{
+    console.log(response)
+})
+
+app.set("view engine","handlebars")
+app.engine('handlebars',handlebars({
+    defaultView:"home",
+    layoutsDir:__dirname+"/views/layouts"
+}))
+
+app.get("/",function(req,res){
+    novelcovid.countries().then(response=>{
+        res.render("home",{info:response})
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("server started")
+})
