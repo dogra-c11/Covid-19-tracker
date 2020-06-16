ce6956e4dd04e6f478d10db618797b37ab1bc700
unknown <gkdogra1111@gmail.com>
2020-06-16 23:26:51 +0530

    total cases with country flags

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-const express=require("express")
+const express = require("express");
 
-const app=express()
-const novelcovid=require("novelcovid")
-const handlebars=require("express-handlebars")
-novelcovid.all().then(response=>{
-    console.log(response)
-})
+const app = express();
+const novelcovid = require("novelcovid");
+const handlebars = require("express-handlebars");
+novelcovid.all().then((response) => {
+  console.log(response);
+});
 
-app.set("view engine","handlebars")
-app.engine('handlebars',handlebars({
-    defaultView:"home",
-    layoutsDir:__dirname+"/views/layouts"
-}))
+app.set("view engine", "handlebars");
+app.engine(
+  "handlebars",
+  handlebars({
+    defaultView: "home",
+    layoutsDir: __dirname + "/views/layouts",
+  })
+);
 
-app.get("/",function(req,res){
-    novelcovid.countries().then(response=>{
-        res.render("home",{info:response})
-    })
-})
+app.get("/", function (req, res) {
+  novelcovid.countries().then((response) => {
+    res.render("home", { info: response });
+  });
+});
 
-app.listen(3000,()=>{
-    console.log("server started")
-})
+app.listen(3000, () => {
+  console.log("server started at port 3000");
+});
